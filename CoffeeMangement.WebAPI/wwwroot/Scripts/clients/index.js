1d386ba783f03cc923e7ce6b05504875539fa1c5
Lê Thiện Đức <lethienduc123@gmail.com>
2020-07-27 18:21:56 +0700

    FirstApp

---
+++ CoffeeMangement.WebAPI/wwwroot/Scripts/clients/index.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../plugins/angular/angular.min.js" />
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", myController)
+myApp.service("Validation", Validation);
+myApp.directive("ngDirective", ngDirective);
+
+myController.$inject = ['$scope', 'Validation'];
+
+
+
+function myController($scope, Validation) {
+    $scope.checkNumber = function () { $scope.message = Validation.checkNumber($scope.num); }
+}
+
+function Validation($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return "Đây là số chẵn"
+        }
+        else {
+            return "Đây là số lẻ"
+        }
+    }
+}
+
+function ngDirective() {
+    return {
+        restrict: "A",
+        template: "<h1>Đây là Directive </h1>"
+    }
+}
