13bb21f2b06341b072086f8fda10e786a752d4fb
Lê Thiện Đức <lethienduc123@gmail.com>
2020-07-28 17:54:42 +0700

    ádasdsa

--- CoffeeMangement.WebAPI/wwwroot/app/app.js
+++ CoffeeMangement.WebAPI/wwwroot/app/app.js
@@ -1,1 +1,13 @@
 ﻿
+(function () {
+    var routerApp = angular.module('routerApp', ['ui.router']);
+    routerApp.config(function($routeProvider, $locationProvider){
+        $routeProvider.caseInsensitiveMatch = true;
+
+        $routeProvider.when('home', {
+            url: '/home',
+            templateUrl: "/components/homes/homeView.html",
+            controller: "homeController"
+        });
+    });
+})();
--- CoffeeMangement.WebAPI/wwwroot/app/components/foods/foodListController.js
+++ CoffeeMangement.WebAPI/wwwroot/app/components/foods/foodListController.js
@@ -1,1 +1,6 @@
-﻿
+﻿(function (app) {
+    app.controller('foodsListController', foodsListController);
+    function foodsListController() {
+
+    }
+})(angular.module('CoffeeManage.foods'))
